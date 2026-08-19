7a46a43cda6157ba1beaa23f008f620922bea337
rdoyle1010 <rebecca.whc@outlook.com>
2026-08-19 15:51:11 +0100

    Link talent applications to placement archive

--- src/app/talent/applications/page.tsx
+++ src/app/talent/applications/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 
+import Link from 'next/link'
 import DashboardShell from '@/components/DashboardShell'
 import TalentApplicationsWorkspace from '@/components/TalentApplicationsWorkspace'
 
 export default function TalentApplicationsPage() {
   return (
     <DashboardShell role="talent">
+      <div className="mb-5 flex justify-end">
+        <Link href="/talent/hired" className="btn-secondary text-[12px]">View hired placements</Link>
+      </div>
       <TalentApplicationsWorkspace />
     </DashboardShell>
   )
