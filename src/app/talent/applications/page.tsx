3f50c6dce320b8176dead15b673dcd82ca915025
rdoyle1010 <rebecca.whc@outlook.com>
2026-08-19 18:28:09 +0100

    Show explicit interview acceptance actions

--- src/app/talent/applications/page.tsx
+++ src/app/talent/applications/page.tsx
@@ -3,10 +3,12 @@
 import Link from 'next/link'
 import DashboardShell from '@/components/DashboardShell'
 import TalentApplicationsWorkspace from '@/components/TalentApplicationsWorkspace'
+import TalentPendingInterviewActions from '@/components/TalentPendingInterviewActions'
 
 export default function TalentApplicationsPage() {
   return (
     <DashboardShell role="talent">
+      <TalentPendingInterviewActions />
       <div className="mb-5 flex justify-end">
         <Link href="/talent/hired" className="btn-secondary text-[12px]">View hired placements</Link>
       </div>
